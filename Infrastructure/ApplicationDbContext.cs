11b0d76aeedfbe2ae3f8c012e335854a64e01ebf
luna-eduardo <eduuardok@gmail.com>
2024-08-18 11:25:14 -0300

    data seed first customer and products

--- Infrastructure/ApplicationDbContext.cs
+++ Infrastructure/ApplicationDbContext.cs
@@ -14,5 +14,33 @@ namespace Infrastructure
         public DbSet<BillingLines> BillingLines { get; set; }
 
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Customer>().HasData(
+                    new Customer
+                    {
+                        Id = Guid.Parse("12081264-5645-407a-ae37-78d5da96fe59"),
+                        Name = "Cliente Exemplo 1",
+                        Email = "cliente1@example.com",
+                        Address =  "Rua Exemplo 1, 123"
+                    }
+                );
+
+            modelBuilder.Entity<Product>().HasData(
+                new Product
+                {
+                    Id = Guid.Parse("48c6dc20-a943-4f8f-83ca-1e1cf094a683"),
+                    Name = "Produto 1"
+                },
+                new Product
+                {
+                    Id = Guid.Parse("48c6dc20-a943-4f8f-83ca-1e1cf094a612"),
+                    Name = "Produto 2"
+                }
+                );
+        }
     }
 }
---
+++ Infrastructure/Migrations/20240818142338_DataSeed_First_Customer_and_Products.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class DataSeed_First_Customer_and_Products : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Customers",
+                columns: new[] { "Id", "Address", "Email", "Name" },
+                values: new object[] { new Guid("12081264-5645-407a-ae37-78d5da96fe59"), "Rua Exemplo 1, 123", "cliente1@example.com", "Cliente Exemplo 1" });
+
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "Id", "Name" },
+                values: new object[,]
+                {
+                    { new Guid("48c6dc20-a943-4f8f-83ca-1e1cf094a612"), "Produto 2" },
+                    { new Guid("48c6dc20-a943-4f8f-83ca-1e1cf094a683"), "Produto 1" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Customers",
+                keyColumn: "Id",
+                keyValue: new Guid("12081264-5645-407a-ae37-78d5da96fe59"));
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: new Guid("48c6dc20-a943-4f8f-83ca-1e1cf094a612"));
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: new Guid("48c6dc20-a943-4f8f-83ca-1e1cf094a683"));
+        }
+    }
+}
